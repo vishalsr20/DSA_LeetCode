872544c5bd2a6a9a50ae77e5b733e43f26c7f6a2
Vishal Subhash Rathod <154747312+vishalsr20@users.noreply.github.com>
2026-08-01 14:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (96.93%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,51 +2,39 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-        int result = -1;
         int low = 0;
         int high = n-1;
 
         while(low <= high){
-            int mid = low + (high - low) / 2;
+            int mid = low  + (high - low) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
             if(nums[mid] > nums[n-1]){
-                low = mid+1;
+                //part 2
+                if(nums[mid] < target){
+                  low = mid+1;
+                }else {
+                    if(nums[0] > target){
+                        low = mid+1;
+                    }else{
+                        high = mid-1;
+                    }
+                }
+
             }else{
-                result = mid;
-                high = mid-1;
+                //part1
+                if(nums[mid] > target){
+                    high = mid-1;
+                }else{
+                    if(nums[n-1] < target){
+                        high = mid-1;
+                    }else{
+                        low = mid+1;
+                    }
+                }
             }
         }
-        int mini = result;
-        low = 0;
-        high = mini;
-        
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }
-
-            if(nums[mid] < target){
-                low = mid+1;
-            }else{
-                high = mid-1;
-            }
-        }
-        low = mini;
-        high = n-1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }
-
-            if(nums[mid] < target){
-                low = mid+1;
-            }else{
-                high = mid-1;
-            }
-        }
-
         return -1;
-        
     }
 };

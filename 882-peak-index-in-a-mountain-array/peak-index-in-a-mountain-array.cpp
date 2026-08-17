e90e6c336103621210e95be4a90c2350c0e86d44
Vishal Subhash Rathod <154747312+vishalsr20@users.noreply.github.com>
2026-08-18 00:08:11 +0530

    Time: 0 ms (100.00%) | Memory: 63.4 MB (98.44%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -4,15 +4,15 @@ public:
         int left = 0;
         int right = arr.size()-1;
         int result = -1;
-        while(left <=  right){
-            int mid = left + (right - left) / 2;
-
-            if(arr[mid] < arr[mid+1] ){
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(arr[mid] < arr[mid+1]){
                 left = mid+1;
             }else{
                 result = mid;
                 right = mid-1;
             }
+            
         }
         return result;
     }
